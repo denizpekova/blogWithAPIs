61a7f2830f2b737c3ffe4e61d0e33d62a0f349ce
Deniz Pekova <denizpekova9@gmail.com>
2026-04-23 19:05:31 +0300

    feat: register BlogRepository and BlogManager services for dependency injection

--- blogWithAPI/Program.cs
+++ blogWithAPI/Program.cs
@@ -1,4 +1,7 @@
 using blogWithAPI.DataAccessLayer.Concrete;
+using blogWithAPI.DataAccessLayer.Abstract;
+using blogWithAPI.BusinessLayer.Abstract;
+using blogWithAPI.BusinessLayer.Concrate;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,9 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogService, BlogManager>();
 
 var app = builder.Build();
 
